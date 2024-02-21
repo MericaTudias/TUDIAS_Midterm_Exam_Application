65b452f99c0b55dcfe64eea1b6e9f86ed11ac60d
Merica Tudias <mericatudias33@gmail.com>
2024-02-21 12:44:28 +0800

    Creating Controller, View, Routes

---
+++ Laravel_Project/resources/views/ProductView.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Products</title>
+</head>
+<body>
+    <h1>MOVIES</h1>
+    <ul>
+        <li>Hunger Games</li>
+        <li>Forest Gump</li>
+        <li>Titanic</li>
+        <li></li>
+    </ul>
+</body>
+</html>
